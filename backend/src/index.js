f43a24424ee91741c93cf5c44a84a3f1abad3484
Sushanta Das <susantadas59713@gmail.com>
2025-02-06 23:10:01 +0530

    Move index.js from src to backend folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser"
 import "dotenv/config";
-import connectWithDatabase from "./src/db/connection.js";
-import userRouter from "./src/routes/user.route.js";
-import tokenRouter from "./src/routes/token.route.js";
-import profileRouter from "./src/routes/profile.route.js";
-import problemRouter from "./src/routes/problem.route.js";
+import connectWithDatabase from "./db/connection.js";
+import userRouter from "./routes/user.route.js";
+import tokenRouter from "./routes/token.route.js";
+import profileRouter from "./routes/profile.route.js";
+import problemRouter from "./routes/problem.route.js";
 
 await connectWithDatabase();
 
