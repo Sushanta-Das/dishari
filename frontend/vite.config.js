533bedffb972685cc9385dec25788e00b6b22a59
Sushanta Das <116519972+Sushanta-Das@users.noreply.github.com>
2025-02-28 12:21:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "http://localhost:3000"
+            "/api": "3.108.54.172:3000"
         }
     },
   
